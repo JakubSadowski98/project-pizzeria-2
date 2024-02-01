0c94a7c3056e623f04bba5894338cd7114075a36
Jakub Sadowski <kuba.sadowsky@gmail.com>
2024-02-01 18:15:52 +0100

    Add page switching

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,8 +1,54 @@
-import { settings, select } from './settings.js';
+import { settings, select, classNames, templates } from './settings.js'; // eslint-disable-line no-unused-vars
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+//import Booking from './components/Booking.js';
 
 const app = { //obiekt, który pomaga w organizacji kodu aplikacji; jego rolą jest tworzenie nowych instancji i ich wykorzystywanie
+  initPages: function(){ //obsługuje podstrony
+    const thisApp = this; // (!) "this" wskazuje na obiekt "app"
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children; //referencja do podstron (dzieci elementu id="pages")
+    thisApp.navLinks = document.querySelectorAll(select.nav.links); //referencja kolekcji do linków ".main-nav a" do przełączania podstron
+
+    const idFromHash = window.location.hash.replace('#/', ''); // właściwośc "window.location.hash" zawiera id podstrony, która jest aktualnie włączona (np. "#/order")
+    let pageMatchingHash = thisApp.pages[0].id; // id podstrony, która ma być włączona dla pierwszego załadowania aplikacji
+    // check if pageMatchingHash has right page id
+    for (let page of thisApp.pages){
+      if(page.id === idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+    thisApp.avctivatePage(pageMatchingHash); // włączenie podstrony o danym id dla pierwszego załadowania aplikacji
+
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        event.preventDefault();
+        const clickedElement = this; // (!) "this" wskazuje na kliknięty link
+        /*get page id from href attribute*/
+        const id = clickedElement.getAttribute('href').replace('#', '');
+        /*run thisApp.activatePage with that id*/
+        thisApp.avctivatePage(id);
+        /*change url hash */
+        window.location.hash = '#/' + id;
+      });
+    }
+  },
+
+  avctivatePage: function(pageId) { // aktywuje (włącza) podstronę o danym id
+    const thisApp = this;
+
+    /*add class active to matching pages, remove from non-matching */
+    for(let page of thisApp.pages) {
+      page.classList.toggle(classNames.pages.active, page.id == pageId); // lub if(age.id == pageId)
+    }
+
+    /*add class active to matching links, remove from non-matching */
+    for(let link of thisApp.navLinks) {
+      link.classList.toggle(classNames.nav.active, link.getAttribute('href') == '# '+ pageId);
+    }
+  },
+
   initMenu: function(){ //metoda, która pośredniczy w tworzeniu instancji wg szablonu klasy "Product", korzystajac z pobranych danych przez "initData"
     const thisApp = this;
 
@@ -45,6 +91,7 @@ const app = { //obiekt, który pomaga w organizacji kodu aplikacji; jego rolą j
   init: function(){ //metoda, która będzie uruchamiać wszystkie pozostałe komponenty strony, za pośrednictwem innych metod z obiektu "app"
     const thisApp = this;
 
+    thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
   },
